f4f21ecd3ef88308744abf053349d2ef623ee0c7
wangkecheng <wkcing@qq.com>
2017-08-16 14:04:45 +0800

    add Test

--- src/indi/javase/concurrent/Test1.java
+++ src/indi/javase/concurrent/Test1.java
@@ -5,7 +5,28 @@ package indi.javase.concurrent;
  */
 public class Test1 {
 
+	public volatile int inc = 0;
+
+	public void increase() {
+		inc++;
+	}
+
 	public static void main(String[] args) {
-		System.out.println ("HelloWorld");
+		final Test1 test1 = new Test1 ();
+		for (int i = 0; i < 10; i++) {
+			new Thread () {
+				@Override
+				public void run() {
+					for (int j = 0; j < 10000; j++) {
+						test1.increase ();
+					}
+				}
+			}.start ();
+		}
+		while (Thread.activeCount () > 2) {	//保证前面的线程都执行完,Thread.activeCount ()是1还是2？？
+			Thread.yield ();
+		}
+		System.out.println(Thread.activeCount ());
+		System.out.println (test1.inc);
 	}
 }
