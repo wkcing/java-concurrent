0bb5c25118e5e64f498d15e7a29f04077de2d67a
wangkecheng <wkcing@qq.com>
2017-08-16 12:08:22 +0800

    add Test

---
+++ src/indi/javase/concurrent/Test1.java
@@ -0,0 +1,11 @@
+package indi.javase.concurrent;
+
+/**
+ * Created by wangkecheng on 2017/8/16.
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println ("HelloWorld");
+	}
+}
